597012174036452fde773eb0615d9bbf944941eb
Rohit Vishwakarma <159112737+rvishwa1709@users.noreply.github.com>
2026-08-26 18:55:37 +0530

    Implement SP-63 v1 changes

--- src/main/java/org/springframework/samples/petclinic/owner/PetTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetTypeFormatter.java
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Component;
 import java.text.ParseException;
 import java.util.Collection;
 import java.util.Locale;
-import java.util.Objects;
 
 /**
  * Instructs Spring MVC on how to parse and print elements of type 'PetType'. Starting
@@ -40,7 +39,7 @@ public class PetTypeFormatter implements Formatter<PetType> {
 
 	public PetTypeFormatter(PetTypeRepository types) {
 		this.types = types;
-	}
+		}
 
 	@Override
 	public String print(PetType petType, Locale locale) {
@@ -52,7 +51,7 @@ public class PetTypeFormatter implements Formatter<PetType> {
 	public PetType parse(String text, Locale locale) throws ParseException {
 		Collection<PetType> findPetTypes = this.types.findPetTypes();
 		for (PetType type : findPetTypes) {
-			if (Objects.equals(type.getName(), text)) {
+			if (text != null && text.equalsIgnoreCase(type.getName())) {
 				return type;
 			}
 		}
