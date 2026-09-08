42e99ca4e6766e42bf4195cd27c6fddd71faa83a
Rohit Vishwakarma <159112737+rvishwa1709@users.noreply.github.com>
2026-09-08 18:55:10 +0530

    Implement SP-77 v1 changes

--- src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
@@ -201,10 +201,10 @@ class PetControllerTests {
 	@Test
 	void processUpdateFormWithSameName() throws Exception {
 		mockMvc.perform(post("/owners/{ownerId}/pets/{petId}/edit", TEST_OWNER_ID, TEST_PET_ID).param("name", "petty") // same
-																										// name
-																										// as
-																										// existing
-																										// pet
+    // name
+    // as
+    // existing
+    // pet
 			.param("type", "hamster")
 			.param("birthDate", "2015-02-12"))
 			.andExpect(status().is3xxRedirection())
