a974e9b51295c48e0938d75fa1af60212d61c031
Rohit Vishwakarma <159112737+rvishwa1709@users.noreply.github.com>
2026-09-09 10:39:34 +0530

    Implement SP-76 v1 changes

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
@@ -8,7 +8,7 @@
  *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,7 +18,6 @@ package org.springframework.samples.petclinic.owner;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -48,8 +47,7 @@ public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 	/**
 	 * Retrieve {@link Owner}s from the data store by telephone.
 	 * @param telephone Value to search for
-	 * @param pageable Pageable
-	 * @return a Page of matching {@link Owner}s
+	 * @return a Page of matching {@link Owner}s (or an empty Page if none found)
 	 */
 	Page<Owner> findByTelephone(String telephone, Pageable pageable);
 
