a83a40975a293dbc0805e85d2e2aeb14251e7169
Rohit Vishwakarma <159112737+rvishwa1709@users.noreply.github.com>
2026-09-09 11:38:53 +0530

    Implement SP-76 v1 changes

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
@@ -18,6 +18,7 @@ package org.springframework.samples.petclinic.owner;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
